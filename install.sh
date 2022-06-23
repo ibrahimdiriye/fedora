b60614589ccac84a8be6f65f9772a371a8e24e60
Ibrahim Diriye <ibrahimstrigger@gmail.com>
2022-06-23 23:35:11 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo dnf update && sudo dnf install wine npm maven ruby speedtest-cli nginx htop glances qbittorrent fish youtube-dl wget curl git vim emacs nginx -y
+
+fish
+chsh -s $(which fish)
+set -U fish_greeting “”
+
